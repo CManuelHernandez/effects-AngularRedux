330b2dca83dc4401b10723d03ff7a5f9822c8cb4
C.Manuel Hernández <cmanuelhm87@gmail.com>
2025-01-13 11:49:18 +0100

    Usuario Service

--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.css'
+  styleUrl: './lista.component.css',
 })
-export class ListaComponent {
+export class ListaComponent implements OnInit {
+  constructor(public usuarioService: UsuarioService) {}
 
+  ngOnInit() {
+    this.usuarioService.getUsers().subscribe((users: any) => {
+      console.log(users);
+    });
+  }
 }
